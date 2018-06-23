e6827fcd0b36e97c39916a5aab5b50db2a0dfdc7
llDanny7 <hdluna.c7@gmail.com>
2018-06-23 13:38:50 +0200

    Subida total

---
+++ src/index.js
@@ -0,0 +1,22 @@
+'use strict'
+const path = require('path')
+const express = require('express');
+const app = express();
+const socketIO = require('socket.io')
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get('/', function(req, res){
+    res.status(200).send({message:"Se ha iniciado correctamente 1"})
+})
+
+
+const server = app.listen(8080, function(){
+    console.log("Se ha iniciado")
+})
+
+const io = socketIO(server)
+
+io.on('connection', (socket)=>{
+    console.log("NUEVA CONEXION", socket.id)
+})
